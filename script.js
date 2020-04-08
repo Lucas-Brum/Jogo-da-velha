cd531652869eced296813a0d3acf5da36df06f52
lucas-brum <lucasmatheusrodriguesbrum@gmail.com>
2020-04-08 17:21:22 -0300

    Correção de bugs

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function carregar ()
 {
     var img = [window.document.getElementById('img00'),
-               window.document.getElementById('img01'),window.document.getElementById('img02'),window.document.getElementById('img10'), window.document.getElementById('img11'),window.document.getElementById('img12'), window.document.getElementById('img20'),window.document.getElementById('img21'),window.document.getElementById('img22')]
-    var okay =  
+               window.document.getElementById('img01'),
+               window.document.getElementById('img02'),
+               window.document.getElementById('img10'),
+               window.document.getElementById('img11'),
+               window.document.getElementById('img12'), 
+               window.document.getElementById('img20'),
+               window.document.getElementById('img21'),
+               window.document.getElementById('img22')]
+    var placar =  
                 [
                     ["00","01","02"],
                     ["10","11","12"],
@@ -28,42 +35,42 @@ function carregar ()
 
 
     function clicar00()
-    {   if(okay[0][0]==="00"&& win ==0)
+    {   if(placar[0][0]==="00"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[0].src = 'Img/Xis.png'
                 player.innerHTML = "Vez de "+p2
-                okay[0][0]="player1"
+                placar[0][0]="player1"
             }
             else
             {
                 vez = vez+1
                 img[0].src = 'Img/Circulo.png'
                 player.innerHTML = "Vez de "+p1
-                okay[0][0]="player2"
+                placar[0][0]="player2"
             }
         }
         vitoria()
     }
     function clicar01()
     {
-        if(okay[0][1]=="01"&& win ==0)
+        if(placar[0][1]=="01"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[1].src = 'Img/Xis.png'
                 player.innerHTML = "Vez de "+p2
-                okay[0][1]="player1"
+                placar[0][1]="player1"
             }
             else
             {
                 vez = vez+1
                 img[1].src = 'Img/Circulo.png'
                 player.innerHTML = "Vez de "+p1
-                okay[0][1]="player2"
+                placar[0][1]="player2"
             }
         }
         vitoria()
@@ -71,156 +78,163 @@ function carregar ()
     }
     function clicar02()
     {   
-        if(okay[0][2]=="02"&& win ==0)
+        if(placar[0][2]=="02"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[2].src = 'Img/Xis.png'
                 player.innerHTML =  "Vez de "+p2
-                okay[0][2]="player1"
+                placar[0][2]="player1"
             }
             else
             {
                 vez = vez+1
                 img[2].src = 'Img/Circulo.png'
                 player.innerHTML =  "Vez de "+p1
-                okay[0][2]="player2"
+                placar[0][2]="player2"
             }
         }
         vitoria()
     }
     function clicar10()
     {
-        if(okay[1][0]=="10" && win ==0)
+        if(placar[1][0]=="10" && win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[3].src = 'Img/Xis.png'
                 player.innerHTML =  "Vez de "+p2
-                okay[1][0]="player1"
+                placar[1][0]="player1"
             }
             else
             {
                 vez = vez+1
                 img[3].src = 'Img/Circulo.png'
                 player.innerHTML =  "Vez de "+p1
-                okay[1][0]="player2"
+                placar[1][0]="player2"
             }
         }
         vitoria()
     }
     function clicar11()
     {
-        if(okay[1][1]=="11"&& win ==0)
+        if(placar[1][1]=="11"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[4].src = 'Img/Xis.png'
                 player.innerHTML =  "Vez de "+p2
-                okay[1][1]="player1"
+                placar[1][1]="player1"
             }
             else
             {
                 vez = vez+1
                 img[4].src = 'Img/Circulo.png'
                 player.innerHTML =  "Vez de "+p1
-                okay[1][1]="player2"
+                placar[1][1]="player2"
             }
         }
         vitoria()
     }
     function clicar12()
     {
-        if(okay[1][2]=="12"&& win ==0)
+        if(placar[1][2]=="12"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[5].src = 'Img/Xis.png'
                 player.innerHTML =  "Vez de "+p2
-                okay[1][2]="player1"
+                placar[1][2]="player1"
             }
             else
             {
                 vez = vez+1
                 img[5].src = 'Img/Circulo.png'
                 player.innerHTML =  "Vez de "+p1
-                okay[1][2]="player2"
+                placar[1][2]="player2"
             }
         }
         vitoria()
     }
     function clicar20()
     {
-        if(okay[2][0]=="20"&& win ==0)
+        if(placar[2][0]=="20"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[6].src = 'Img/Xis.png'
                 player.innerHTML =  "Vez de "+p2
-                okay[2][0]="player1"
+                placar[2][0]="player1"
             }
             else
             {
                 vez = vez+1
                 img[6].src = 'Img/Circulo.png'
                 player.innerHTML =  "Vez de "+p1
-                okay[2][0]="player2"
+                placar[2][0]="player2"
             }
         }
         vitoria()
     }
     function clicar21()
     {
-        if(okay[2][1]=="21"&& win ==0)
+        if(placar[2][1]=="21"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[7].src = 'Img/Xis.png'
                 player.innerHTML = "Vez de "+p2
-                okay[2][1]="player1"
+                placar[2][1]="player1"
             }
             else
             {
                 vez = vez+1
                 img[7].src = 'Img/Circulo.png'
                 player.innerHTML = "Vez de "+p1
-                okay[2][1]="player2"
+                placar[2][1]="player2"
             }
         }
         vitoria()
 }
     function clicar22()
     {
-        if(okay[2][2]=="22"&& win ==0)
+        if(placar[2][2]=="22"&& win ==0)
         {
             if (vez % 2 == 0)
             {
                 vez = vez+1
                 img[8].src = 'Img/Xis.png'
                 player.innerHTML = "Vez de "+p2
-                okay[2][2]="player1"
+                placar[2][2]="player1"
             }
             else
             {
                 vez = vez+1
                 img[8].src = 'Img/Circulo.png'
                 player.innerHTML = "Vez de "+p1
-                okay[2][2]= "player2"
+                placar[2][2]= "player2"
             }
         }
         vitoria()
     }
     function vitoria()
     {
-        if(okay[0][0] == okay[1][1] && okay[0][0] ==okay[2][2])
+                                                                                           //Determina se é empate
+        if(vez>=9 && (player1<3 || player2<3))
+                  {
+                    win=1
+                    player.innerHTML = " Empate"
+                  } 
+
+        if(placar[0][0] == placar[1][1] && placar[0][0] ==placar[2][2])
         {
-            if(okay[1][1]=="player1")
+            if(placar[1][1]=="player1")
             {
                 player.innerHTML = p1+"  win!"
                 win =1
@@ -231,9 +245,9 @@ function carregar ()
                 win =1
             }
         }
-        if(okay[0][2]==okay[1][1] && okay[0][2] == okay[2][0])
+        if(placar[0][2]==placar[1][1] && placar[0][2] == placar[2][0])
         {
-            if(okay[1][1]=="player1")
+            if(placar[1][1]=="player1")
             {
                 player.innerHTML = p1+"  win!"
                 win =1
@@ -250,11 +264,11 @@ function carregar ()
             for(var j = 0; j<=2; j++)
             {
    
-               if( okay[i][j] == "player2")
+               if( placar[i][j] == "player2")
                 {
                     player2 ++
                 }
-                if( okay[i][j] == "player1" )
+                if( placar[i][j] == "player1" )
                 {
                     player1 ++
                 }             
@@ -279,11 +293,11 @@ function carregar ()
         {
             for(var i = 0; i<=2; i++)
             {
-               if( okay[i][j] == "player2")
+               if( placar[i][j] == "player2")
                 {
                     player2 ++
                 }
-                if( okay[i][j] == "player1" )
+                if( placar[i][j] == "player1" )
                 {
                     player1 ++
                 }             
@@ -303,14 +317,7 @@ function carregar ()
                     player1 = 0
                     player2 = 0
                 }   
-        }
-                                                                                   //Determina se é empate
-        if(vez>=9 && (player1<3 || player2<3))
-                  {
-                    win=1
-                    player.innerHTML = " Empate"
-                  } 
-
+            }
 
     }
 }
